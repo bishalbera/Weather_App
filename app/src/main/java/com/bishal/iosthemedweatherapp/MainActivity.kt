acbd36259d4502f19499a4540843e7929ff108ae
Bishal <bishalbera2@gmail.com>
2023-06-28 10:03:40 +0530

    viewmodel added

--- app/src/main/java/com/bishal/iosthemedweatherapp/MainActivity.kt
+++ app/src/main/java/com/bishal/iosthemedweatherapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.bishal.iosthemedweatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +13,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bishal.iosthemedweatherapp.presentation.WeatherViewModel
 import com.bishal.iosthemedweatherapp.ui.theme.IosThemedWeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+
+        ))
         setContent {
             IosThemedWeatherAppTheme {
 
